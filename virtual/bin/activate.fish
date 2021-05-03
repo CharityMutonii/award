3f3c767ae2ad4a5498924afe6626baf3fd251b04
Charity Mutoni <charitytoni0@gmail.com>
2021-05-03 23:25:40 +0200

    installed psycopg2

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/charity/Desktop/wecode/Python/Django/IP/Awards/virtual"
+set -gx VIRTUAL_ENV "/home/charity/Desktop/wecode/Python/Django/IP/Award/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
